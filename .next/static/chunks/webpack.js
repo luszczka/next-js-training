b8cc19f28f8015d0994e38594843977b62c6dded
Karolina Łuszcz <karolina@MacBook-Pro-Liki.local>
2022-11-12 22:32:23 +0100

    add packages

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5462a84952f39060"; }
+/******/ 		__webpack_require__.h = function() { return "c301d70b74060f1b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
