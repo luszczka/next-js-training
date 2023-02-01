a1291939b25dd2499d27506a6adfc1b51a9f4e44
Karolina Łuszcz <karolina@MacBook-Pro-Liki.local>
2023-02-01 07:28:16 +0100

    package update

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a55ef839e45b51ea"; }
+/******/ 		__webpack_require__.h = function() { return "dd528168ddcf85da"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
